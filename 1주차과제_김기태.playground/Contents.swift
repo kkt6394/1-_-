6cbddb051ccff2fd8376dbe796d168bb7bf0150d
Aiden <kkt6394@gmail.com>
2025-03-13 10:52:07 +0900

    level1 completed

--- 1주차과제_김기태.playground/Contents.swift
+++ 1주차과제_김기태.playground/Contents.swift
@@ -1,1 +1,22 @@
 // START!!
+
+class Calculator {
+    func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+    func subtract(_ a: Int, _ b: Int) -> Int {
+        return a - b
+    }
+    func multiply(_ a: Int, _ b: Int) -> Int {
+        return a * b
+    }
+    func divide(_ a: Int, _ b: Int) -> Int {
+        return a / b
+    }
+}
+let calculator = Calculator()
+print(calculator.add(10, 20))
+print(calculator.subtract(10, 20))
+print(calculator.multiply(10, 20))
+print(calculator.divide(10, 20))
+
